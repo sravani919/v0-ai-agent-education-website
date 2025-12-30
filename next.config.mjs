b04ed4708f111c15d223c42b6d7febe6b1802837
Sravani Pati <55269953+sravani919@users.noreply.github.com>
2025-12-29 23:56:09 -0500

    Set output to 'export' for GitHub Pages support

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",   // REQUIRED for GitHub Pages
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
- 
 }
 
 export default nextConfig
